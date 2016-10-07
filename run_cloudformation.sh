90cc1b75830d58ce8bd001efcee7e3e59ee38953
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2016-10-08 02:48:36 +0300

    Add waiting stub to init script

--- run_cloudformation.sh
+++ run_cloudformation.sh
@@ -15,4 +15,6 @@ aws cloudformation validate-template --template-body file://create_rds_db.json
 
 aws cloudformation create-stack --stack-name $stack --template-body file://create_rds_db.json --parameters ParameterKey=DBUser,ParameterValue=$username ParameterKey=DBPassword,ParameterValue=$password
 
+#while ! nc $CIP 5432; do sleep 0.1; done; echo '[OK] Db is up!' # Because postgres doesn't accept connections while booting
+
 echo "Finished."

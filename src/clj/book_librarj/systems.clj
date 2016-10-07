455c41ba95873f38eb5d6791c96006ff8345baa1
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2016-10-07 21:13:53 +0300

    Add hook for postgresql

--- src/clj/book_librarj/systems.clj
+++ src/clj/book_librarj/systems.clj
@@ -2,11 +2,22 @@
   (:gen-class)
   (:require [system.core :refer [defsystem]]
             (system.components
-              [http-kit :refer [new-web-server]])
+              [http-kit :refer [new-web-server]]
+              [postgresql :refer [new-postgres-database]])
             [book-librarj.handler :refer [app]]))
+
+(def db-spec
+  {:classname   "org.postgresql.Driver"
+   :subprotocol "postgresql"
+   :subname "system_test_db"
+   :user    "system_test_user"
+   :password ""
+   :host "127.0.0.1"})
 
 (defsystem dev-system
            [:web (new-web-server 3000 app)])
+            ;:db (new-postgres-database db-spec)])
 
 (defsystem prod-system
-           [:web (new-web-server 3000 app)])
+           [:web (new-web-server 3000 app)
+            :db (new-postgres-database db-spec)])

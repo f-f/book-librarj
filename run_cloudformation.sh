d58dc6a5963fcc569a4732dd07171ef84e9d412d
Jouni Leino <jouni.leino@siili.com>
2016-10-08 05:41:37 +0300

    Unfinished crap

--- run_cloudformation.sh
+++ run_cloudformation.sh
@@ -4,17 +4,24 @@ echo "Starting to run CloudFormation template"
 
 echo -n "Enter stack name and press [ENTER]: "
 read stack
-
+#stack=ClojureHackathon
 echo -n "Enter username and press [ENTER]: "
 read username
-
 echo -n "Enter password and press [ENTER]: "
 read password
 
 aws cloudformation validate-template --template-body file://create_rds_db.json
 
-aws cloudformation create-stack --stack-name $stack --template-body file://create_rds_db.json --parameters ParameterKey=DBUser,ParameterValue=$username ParameterKey=DBPassword,ParameterValue=$password
+STACK_ID=$(aws cloudformation create-stack --stack-name $stack --template-body file://create_rds_db.json --parameters ParameterKey=DBUser,ParameterValue=$username ParameterKey=DBPassword,ParameterValue=$password | jq '.StackId')
 
-#while ! nc $CIP 5432; do sleep 0.1; done; echo '[OK] Db is up!' # Because postgres doesn't accept connections while booting
+STACK_ID=arn:aws:cloudformation:eu-west-1:946278600493:stack/ClojureHackathon/10c0a880-8cf0-11e6-9093-500c3d40ec36
+echo $STACK_ID
+
+DB_IDENTIFIER=$(aws cloudformation describe-stack-resource --stack-name=ClojureHackathon --logical-resource-id=MyDB | jq '.StackResourceDetail.PhysicalResourceId')
+echo $DB_IDENTIFIER
+
+aws rds describe-db-instances --db-instance-identifier=$DB_IDENTIFIER
+
+
 
 echo "Finished."
